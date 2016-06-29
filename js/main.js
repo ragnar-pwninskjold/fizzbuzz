6b7386a04f95e6c861138f12efe101e26185b4ce
ragnar-pwninskjold <seankochel@gmail.com>
2016-06-29 17:59:28 -0400

    added refactoring for functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,59 @@
 $(document).ready(function(){
 
-	for (i = 1; i < 100; i++) {
+	//on page load, prompt for #
+
+	do {
+
+		input = prompt("Enter a valid number between 1 and 100 to check for FizzBuzness");
+	}
+
+	while (checker(input) == false);
+
+	// take argument from prompt and do the fizzbuzz
+
+
+	fizzBuzz(input);
+}); 
+
+//make a function that will check if they actually entered a valid number
+
+		//make sure the user is actually entering a number, not a world like "hello"
+
+		//make sure they aren't entering a decimal value
+
+		//method of converting input to an integer parseInt() could work
+
+function checker(x) {
+
+	var y = parseInt(x);
+
+	if (isNaN(parseInt(x)) == true) {
+	
+		return false;
+
+	}
+
+	else if (y % 1 != 0) {
+
+		return false;
+	}
+
+	else {
+
+		return true;
+
+	}
+
+}
+
+//make a function that checks for FizzBuzz
+
+function fizzBuzz (limit) {
+
+//do fizzBuzz while counter < limit, also document write this to the screen each time
+
+
+for (i = 1; i < limit; i++) {
 
 		if (i % 3 == 0 && i % 5 == 0) {
 			document.write("FizzBuzz" + "<br>"+ "<br>");
@@ -16,4 +69,5 @@ $(document).ready(function(){
 		}
 	}	
 
-}); 
+
+}
